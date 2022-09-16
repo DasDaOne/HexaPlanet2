b5174924709bf741baae43f163de291cc7e61d39
DasDaOne <mr.ars355@GMAIL.COM>
2022-09-16 06:47:03 +0400

    [add] Planet model, URP

---
+++ Assets/_Scripts/PlanetGenerator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform groundParent;
+
+    [SerializeField] private Transform waterParent; 
+    
+    private List<int> deletedIndexes = new List<int>();
+    
+    private void Start()
+    {
+        for (int i = 0; i < groundParent.childCount; i++)
+        {
+            if (Random.Range(0, 2) == 0)
+            {
+                deletedIndexes.Add(i);
+                Destroy(groundParent.GetChild(i).gameObject);
+            }
+            else
+            {
+                Destroy(waterParent.GetChild(i).gameObject);
+            }
+        }
+    }
+}
